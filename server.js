051acec484decf3ba17a53268b2d03cc479609e4
Omer Herera <omerh@liveperson.com>
2014-11-24 11:58:04 +0200

    renamed test folder to unt-tests

--- server.js
+++ server.js
@@ -1,99 +1,216 @@
-var req = [
-        {
-            name: "batch1",
-            url: "https://mono",
-            method: "POST",
-            encoding: "UTF8",
-            retries: 3,
-            headers: {},
-            doNotMergeHeaders: false,
-            query: "/user1",
-            mimeType: "application/json",
-            body: "body",
-            timeout: 10000
-        },
-        {
-            name: "batch2",
-            url: "https://jsonresponser.herokuapp.com/api/json/user/1",
-            method: "GET",
-            encoding: "UTF8",
-            retries: 3,
-            headers: {},
-            doNotMergeHeaders: false,
-            query: "/user1",
-            mimeType: "application/json",
-            body: "body",
-            timeout: 1
-        },
-        {
-            name: "batch3",
-            url: "https://jsonresponser.herokuapp.com/api/json/user/2",
-            method: "GET",
-            encoding: "UTF8",
-            retries: 3,
-            headers: {},
-            doNotMergeHeaders: false,
-            query: "/user1",
-            mimeType: "application/json",
-            body: "body",
-            timeout: 10000
-        },
-        {
-            name: "batch4",
-            url: "https://jsonresponser.herokuapp.com/api/json/users",
-            method: "POST",
-            encoding: "UTF8",
-            retries: 3,
-            headers: {},
-            doNotMergeHeaders: false,
-            query: "/user1",
-            mimeType: "application/json",
-            body: "body",
-            timeout: 10000
-        },
-        {
-            name: "batch5",
-            url: "https://jsonresponser.herokuapp.com/api/json/user/1",
-            method: "GET",
-            encoding: "UTF8",
-            retries: 3,
-            headers: {},
-            doNotMergeHeaders: false,
-            query: "/user1",
-            mimeType: "application/json",
-            body: "body",
-            timeout: 10000
-        },
-        {
-            // missing METHOD
-            name: "missing METHOD",
-            url: "https://jsonresponser.herokuapp.com/api/json/user/2",
-            encoding: "UTF8",
-            retries: 3,
-            headers: {},
-            doNotMergeHeaders: false,
-            query: "/user1",
-            mimeType: "application/json",
-            body: "body",
-            timeout: 10000
-        }
+var arrayReqs =
+    [
+            {
+                name: "batch1",
+                url: "https://mono",
+                method: "POST",
+                encoding: "UTF8",
+                retries: 3,
+                headers: {},
+                doNotMergeHeaders: false,
+                query: "/user1",
+                mimeType: "application/json",
+                body: "body",
+                timeout: 10000
+            },
+            {
+                name: "batch2",
+                url: "http://localhost:7777/?id=1",
+                method: "GET",
+                encoding: "UTF8",
+                retries: 3,
+                headers: {},
+                doNotMergeHeaders: false,
+                query: "/user1",
+                mimeType: "application/json",
+                body: "body",
+                timeout: 1
+            },
+            {
+                name: "batch3",
+                url: "http://localhost:7777/?id=2",
+                method: "GET",
+                encoding: "UTF8",
+                retries: 3,
+                headers: {},
+                doNotMergeHeaders: false,
+                query: "/user1",
+                mimeType: "application/json",
+                body: "body",
+                timeout: 10000
+            },
+            {
+                name: "batch4",
+                url: "http://localhost:7777/?id=3",
+                method: "POST",
+                encoding: "UTF8",
+                retries: 3,
+                headers: {},
+                doNotMergeHeaders: false,
+                query: "/user1",
+                mimeType: "application/json",
+                body: "body",
+                timeout: 10000
+            },
+            {
+                name: "batch5",
+                url: "http://localhost:7777/?id=4",
+                method: "GET",
+                encoding: "UTF8",
+                retries: 3,
+                headers: {},
+                doNotMergeHeaders: false,
+                query: "/user1",
+                mimeType: "application/json",
+                body: "body",
+                timeout: 10000
+            },
+            {
+                // missing METHOD
+                name: "missing METHOD",
+                url: "http://localhost:7777/?id=5",
+                encoding: "UTF8",
+                retries: 3,
+                headers: {},
+                doNotMergeHeaders: false,
+                query: "/user1",
+                mimeType: "application/json",
+                body: "body",
+                timeout: 10000
+            }
+    ];
 
-    ]
-    ;
+//var reqSingle = [{
+//    name: "single",
+//    url: "https://jsonresponser.herokuapp.com/api/json/users",
+////    method: "GET",
+//    encoding: "UTF8",
+//    retries: 3,
+//    headers: {},
+//    doNotMergeHeaders: false,
+//    query: "/user1",
+//    mimeType: "application/json",
+//    body: "body",
+//    timeout: 10000
+//}]
 
-var reqSingle = [{
-    name: "single",
-    url: "https://jsonresponser.herokuapp.com/api/json/users",
+
+
+
+//];
+
+//var req = [
+//        {
+//            name: "batch1",
+//            url: "https://mono",
+//            method: "POST",
+//            encoding: "UTF8",
+//            retries: 3,
+//            headers: {},
+//            doNotMergeHeaders: false,
+//            query: "/user1",
+//            mimeType: "application/json",
+//            body: "body",
+//            timeout: 10000
+//        },
+//        {
+//            name: "batch2",
+//            url: "https://jsonresponser.herokuapp.com/api/json/user/1",
+//            method: "GET",
+//            encoding: "UTF8",
+//            retries: 3,
+//            headers: {},
+//            doNotMergeHeaders: false,
+//            query: "/user1",
+//            mimeType: "application/json",
+//            body: "body",
+//            timeout: 1
+//        },
+//        {
+//            name: "batch3",
+//            url: "https://jsonresponser.herokuapp.com/api/json/user/2",
+//            method: "GET",
+//            encoding: "UTF8",
+//            retries: 3,
+//            headers: {},
+//            doNotMergeHeaders: false,
+//            query: "/user1",
+//            mimeType: "application/json",
+//            body: "body",
+//            timeout: 10000
+//        },
+//        {
+//            name: "batch4",
+//            url: "https://jsonresponser.herokuapp.com/api/json/users",
+//            method: "POST",
+//            encoding: "UTF8",
+//            retries: 3,
+//            headers: {},
+//            doNotMergeHeaders: false,
+//            query: "/user1",
+//            mimeType: "application/json",
+//            body: "body",
+//            timeout: 10000
+//        },
+//        {
+//            name: "batch5",
+//            url: "https://jsonresponser.herokuapp.com/api/json/user/1",
+//            method: "GET",
+//            encoding: "UTF8",
+//            retries: 3,
+//            headers: {},
+//            doNotMergeHeaders: false,
+//            query: "/user1",
+//            mimeType: "application/json",
+//            body: "body",
+//            timeout: 10000
+//        },
+//        {
+//            // missing METHOD
+//            name: "missing METHOD",
+//            url: "https://jsonresponser.herokuapp.com/api/json/user/2",
+//            encoding: "UTF8",
+//            retries: 3,
+//            headers: {},
+//            doNotMergeHeaders: false,
+//            query: "/user1",
+//            mimeType: "application/json",
+//            body: "body",
+//            timeout: 10000
+//        }
+//
+//    ]
+//    ;
+
+//var reqSingle = [{
+//    name: "single",
+//    url: "https://jsonresponser.herokuapp.com/api/json/users",
+////    method: "GET",
+//    encoding: "UTF8",
+//    retries: 3,
+//    headers: {},
+//    doNotMergeHeaders: false,
+//    query: "/user1",
+//    mimeType: "application/json",
+//    body: "body",
+//    timeout: 10000
+//}];
+//
+//var reqRepeat = [{
+//    name: "single",
+//    url: "http://localhost:7777/?id=1",
 //    method: "GET",
-    encoding: "UTF8",
-    retries: 3,
-    headers: {},
-    doNotMergeHeaders: false,
-    query: "/user1",
-    mimeType: "application/json",
-    body: "body",
-    timeout: 10000
-}];
+//    encoding: "UTF8",
+//    retries: 3,
+//    headers: {},
+//    doNotMergeHeaders: false,
+//    query: "/user1",
+//    mimeType: "application/json",
+//    body: "body",
+//    timeout: 10000
+//}];
+
+
 
 var config = require('./config/config.json');
 config.logger = console;
@@ -105,10 +222,16 @@ batchelor.configure(config);
 //    console.log("---->>>>Last Callback1111: "+ JSON.stringify(results));
 //});
 
-var jobId2 = batchelor.execute(reqSingle, function (err, results) {
+function repeat () {
+    var jobId2 = batchelor.execute(arrayReqs, function (err, results) {
 //    console.log("Last Callback: "+ results);
-    console.log("CallbackSingle Response: "+ JSON.stringify(results));
-});
+        console.log("CallbackSingle Response: "+ JSON.stringify(results));
+    });
+}
+
+setInterval(function () {
+    repeat();
+}, 5000);
 
 //console.log("JOBID1: " + jobId);
 //console.log("JOBID2: " + jobId2);
