d69b4232aa34f16fb09f1cb9819e0cac01b1ee4d
Omer Herera <omerh@liveperson.com>
2015-01-26 08:34:31 +0200

    remove comments addded by mistake

--- media/index.js
+++ media/index.js
@@ -19,14 +19,3 @@ var util        = require("util")
     util.inherits(Media, events.EventEmitter);
     module.exports = new Media();
 })();
-
-
-//var stream = new MyStream();
-//
-//console.log(stream instanceof events.EventEmitter); // true
-//console.log(MyStream.super_ === events.EventEmitter); // true
-//
-//stream.on("data", function(data) {
-//    console.log('Received data: "' + data + '"');
-//})
-//stream.write("It works!"); // Received data: "It works!"
