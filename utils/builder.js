9b1507b3ff6d8140c9fc0b03cd8512141a3abd8c
Omer Herera <omer.herera@gmail.com>
2015-07-14 10:34:55 +0300

    request_response_refactor

--- utils/builder.js
+++ utils/builder.js
@@ -6,34 +6,42 @@ var config
 var RESPONSE_TYPE = {
     INVALID_TASK: {
         body: commons.CONST.BODY_RESPONSE.INVALID_TASK,
+        HTTPStatus: commons.CONST.HTTP_STATUS.BAD_REQUEST,
         statusCode: commons.CONST.HTTP_STATUS.BAD_REQUEST
     },
     ETIMEDOUT: {
         body: commons.CONST.BODY_RESPONSE.ETIMEDOUT,
+        HTTPStatus: commons.CONST.HTTP_STATUS.GATEWAY_TIMEOUT,
         statusCode: commons.CONST.HTTP_STATUS.GATEWAY_TIMEOUT
     },
     ECONNREFUSED: {
         body: commons.CONST.BODY_RESPONSE.ECONNREFUSED,
+        HTTPStatus: commons.CONST.HTTP_STATUS.NOT_FOUND,
         statusCode: commons.CONST.HTTP_STATUS.NOT_FOUND
     },
     ENOTFOUND: {
         body: commons.CONST.BODY_RESPONSE.ENOTFOUND,
+        HTTPStatus: commons.CONST.HTTP_STATUS.NOT_FOUND,
         statusCode: commons.CONST.HTTP_STATUS.NOT_FOUND
     },
     ERROR_API_URL: {
         body: commons.CONST.BODY_RESPONSE.ERROR_API_URL,
+        HTTPStatus: commons.CONST.HTTP_STATUS.INTERNAL_SERVER_ERROR,
         statusCode: commons.CONST.HTTP_STATUS.INTERNAL_SERVER_ERROR
     },
     NO_JSON_OBJECT: {
         body: commons.CONST.BODY_RESPONSE.NO_JSON_OBJECT,
+        HTTPStatus: commons.CONST.HTTP_STATUS.BAD_REQUEST,
         statusCode: commons.CONST.HTTP_STATUS.BAD_REQUEST
     },
     NON_PERSISTENT_REQUEST: {
         body: commons.CONST.BODY_RESPONSE.NON_PERSISTENT_REQUEST,
+        HTTPStatus: commons.CONST.HTTP_STATUS.NOT_ACCEPTABLE,
         statusCode: commons.CONST.HTTP_STATUS.NOT_ACCEPTABLE
     },
     DEFAULT: {
-        body: commons.CONST.BODY_RESPONSE.DEFAULT,
+        body: commons.CONST.BODY_RESPONSE.DEFAULT, 
+        HTTPStatus: commons.CONST.HTTP_STATUS.NOT_FOUND,
         statusCode: commons.CONST.HTTP_STATUS.NOT_FOUND
     }
 
