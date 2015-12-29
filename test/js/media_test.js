71ffd5661bfebee26f9e34956dfd3368c1866051
michaeld <michaeld@liveperson.com>
2015-12-29 09:05:32 +0200

    simplified media and added test

--- test/js/media_test.js
+++ test/js/media_test.js
@@ -4,7 +4,16 @@ var Media = requireHelper('media');
 
 describe('Media', function () {
 
-    it('object was created', function () {
+    it('should exist and be an object', function () {
         expect(Media).to.be.an('object');
+    });
+
+    it('should emit properly', function (done) {
+        const event = "testEvent";
+        Media.on(event, function (data) {
+            expect(data).to.equal(1);
+            done();
+        });
+        Media.emit(event, 1);
     });
 });
