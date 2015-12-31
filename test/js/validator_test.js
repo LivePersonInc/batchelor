ed57a034e32abed451fc7f420997e02908bb1af6
michaeld <michaeld@liveperson.com>
2015-12-31 18:14:43 +0200

    fixed issued with white list, validator, added tests

--- test/js/validator_test.js
+++ test/js/validator_test.js
@@ -1,11 +1,32 @@
 var expect = require('chai').expect;
 var requireHelper = require('./util/require_helper');
-var log = requireHelper('util/Validator');
+var validator = requireHelper('util/Validator');
+var conf = requireHelper('util/Config');
 
 describe('Validator Tests', function () {
 
-    it('should validate', function () {
+    after(function () {
+        conf.setConf({
+            whiteList: ["*"]
+        });
+    });
 
+    it('should not validate request without name/url', function () {
+        expect(validator.isValidRequest({shmoop: "myName"})).to.be.false;
+        expect(validator.isValidRequest({name: "myName"})).to.be.false;
+        expect(validator.isValidRequest({url: "http://www.mySite.com"})).to.be.false;
+    });
+
+    it('should validate a valid request', function () {
+        expect(validator.isValidRequest({name: "myName", url: "http://www.mySite.com"})).to.be.true;
+    });
+
+    it('should validate url against white list', function () {
+        conf.setConf({
+            whiteList: ["myothersite.com"]
+        });
+        expect(validator.isValidRequest({name: "myName", url: "http://www.mySite.com"})).to.be.false;
+        expect(validator.isValidRequest({name: "myName", url: "http://www.myOtherSite.com"})).to.be.true;
     });
 
 });
