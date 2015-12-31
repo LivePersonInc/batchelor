fa5dfee6e7929286887c2b47866b7d8bc9ab677e
michaeld <michaeld@liveperson.com>
2015-12-31 16:46:01 +0200

    Completely redesign and refactor batchelor, new API

--- test/js/events_test.js
+++ test/js/events_test.js
@@ -1,19 +1,19 @@
 var expect = require('chai').expect;
 var requireHelper = require('./util/require_helper');
-var Media = requireHelper('media');
+var Events = requireHelper('Events');
 
-describe('Media', function () {
+describe('Events Tests', function () {
 
     it('should exist and be an object', function () {
-        expect(Media).to.be.an('object');
+        expect(Events).to.be.an('object');
     });
 
     it('should emit properly', function (done) {
         const event = "testEvent";
-        Media.on(event, function (data) {
+        Events.on(event, function (data) {
             expect(data).to.equal(1);
             done();
         });
-        Media.emit(event, 1);
+        Events.emit(event, 1);
     });
 });
